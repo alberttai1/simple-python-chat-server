59d31c9022d5c640b81a38bea26257bdda355794
Tzu Yu Tai <ttai3@uwo.ca>
2016-02-17 12:34:12 -0500

    finished a prototype backend

---
+++ server.py
@@ -0,0 +1,46 @@
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+
+class IphoneChat(Protocol):
+
+    # Deals when a client is connected
+    def connectionMade(self):
+        self.factory.clients.append(self)
+        print "clients are ", self.factory.clients
+
+    # Deals when a client disconnects
+    def connectionLost(self, reason):
+        self.factory.clients.remove(self)
+
+    # Deals with data sent
+    def dataReceived(self, data):
+        a = data.split(':')
+        print a
+        if len(a) > 1:
+            command = a[0]
+            content = a[1]
+
+            msg = ""
+            if command == "iam":
+                self.name = content
+                msg = self.name + " has joined"
+
+            elif command == "msg":
+                msg = self.name + ": " + content
+                print msg
+
+            for clients in self.factory.clients:
+                clients.message(msg)
+
+    # Deals with Messages
+    def message(self, message):
+        self.transport.write(message + '\n')
+
+# Handles Incoming Connections
+factory = Factory()
+factory.clients = []
+factory.protocol = IphoneChat
+
+reactor.listenTCP(80, factory)
+print "Iphone Chat server started"
+reactor.run()
